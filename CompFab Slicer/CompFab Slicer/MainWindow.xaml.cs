c6bcb301c6ae503f8e3e6ccb4d57dce14c755c89
unknown <goosrobin1@gmail.com>
2020-11-07 12:35:54 +0100

    cleaned up code massively, changed approach to slicing

--- CompFab Slicer/CompFab Slicer/MainWindow.xaml.cs
+++ CompFab Slicer/CompFab Slicer/MainWindow.xaml.cs
@@ -16,17 +16,18 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using HelixToolkit.Wpf;
 using Microsoft.Win32;
-using ClipperLib;
-
-using Path = System.Collections.Generic.List<ClipperLib.IntPoint>;
-using Paths = System.Collections.Generic.List<System.Collections.Generic.List<ClipperLib.IntPoint>>;
-using System.Runtime.InteropServices;
-using Polygon3D = HelixToolkit.Wpf.Polygon3D;
 
 namespace CompFab_Slicer
 {
     public partial class MainWindow : Window
     {
+        public MeshBuilder meshBuilder = new MeshBuilder(false, false);
+        public MeshGeometry3D modelMesh;
+
+        Material yellowMaterial = MaterialHelper.CreateMaterial(Colors.Yellow);
+        Material insideMaterial = MaterialHelper.CreateMaterial(Colors.Gray);
+
+        Model3DGroup modelGroup = new Model3DGroup();
         public MainWindow()
         {
             InitializeComponent();
@@ -47,108 +48,19 @@ namespace CompFab_Slicer
 
         public void ShowSTL(string file)
         {
-            var modelGroup = new Model3DGroup();
             var visualModel = VisualModel;
-
-            // Create a mesh builder and add a box to it
-             var meshBuilder = new MeshBuilder(false, false);
-            //meshBuilder.AddBox(new Point3D(0, 0, 0), 50, 50, 0.2);
-            // Create a mesh from the builder (and freeze it
-
-            // Create some materials
-            var yellowMaterial = MaterialHelper.CreateMaterial(Colors.Yellow);
-            var insideMaterial = MaterialHelper.CreateMaterial(Colors.Gray);
 
             StLReader reader = new StLReader();
             Model3DGroup group = reader.Read(file);
             GeometryModel3D geoModel = FindLargestModel(group);
-            MeshGeometry3D stlMesh = geoModel.Geometry as MeshGeometry3D;
-
-            Int32Collection indices = stlMesh.TriangleIndices;
-
-            Paths contours = new Paths();
-            Paths clip = new Paths();
-            int scale = 10000;
-
-            for(int i = 0; i < stlMesh.TriangleIndices.Count; i += 3)
-            {
-                Path contour = new Path(3);
-
-                Point3D vec_1 = stlMesh.Positions[indices[i]];
-                Point3D vec_2 = stlMesh.Positions[indices[i + 1]];
-                Point3D vec_3 = stlMesh.Positions[indices[i + 2]];
-
-                contour.Add(new IntPoint((long)(vec_1.X *scale), (long)(vec_1.Y * scale), (long)(vec_1.Z * scale)));
-                contour.Add(new IntPoint((long)(vec_2.X *scale), (long)(vec_2.Y * scale), (long)(vec_2.Z * scale)));
-                contour.Add(new IntPoint((long)(vec_3.X *scale), (long)(vec_3.Y * scale), (long)(vec_3.Z * scale)));
-
-                contours.Add(contour);   
-            }
-
-            Path zPlane = new Path(4);
-            zPlane.Add(new IntPoint(-100 * scale, -100 * scale, 22 * scale));
-            zPlane.Add(new IntPoint(-100 * scale, 100 * scale, 22 * scale));
-            zPlane.Add(new IntPoint(100 * scale, 100 * scale, 22 * scale));
-            zPlane.Add(new IntPoint(100 * scale, -100 * scale, 22 * scale));
-            clip.Add(zPlane);
+            modelMesh = geoModel.Geometry as MeshGeometry3D;
 
             geoModel.Material = yellowMaterial;
             geoModel.BackMaterial = insideMaterial;
             geoModel.Transform = new TranslateTransform3D(0, 0, 0);
             modelGroup.Children.Add(geoModel);
 
-            Paths slice = intersect(contours, clip, 0.2, scale);
-            
-
-            for(int i = 0; i < slice.Count; i++)
-            {
-                List<Point3D> polygon = new List<Point3D>();
-                for (int j = 0; j < slice[i].Count; j++)
-                {
-                    if(0*scale <= slice[i][j].Z && slice[i][j].Z <= (long)(0.2*scale))
-                    {
-                        Point3D temp = new Point3D((double)(slice[i][j].X) / scale, (double)(slice[i][j].Y) / scale, 0);
-                        polygon.Add(temp);
-                    }
-                }
-                meshBuilder.AddPolygon(polygon);
-            }
-            var mesh = meshBuilder.ToMesh(true);
-
-
-            GeometryModel3D testModel = new GeometryModel3D { Geometry = mesh, Material = yellowMaterial, BackMaterial = insideMaterial, Transform = new TranslateTransform3D(0, 50, 0) };
-            modelGroup.Children.Add(testModel);
-
             visualModel.Content = modelGroup;
-        }
-
-        public Paths intersect(Paths contours, Paths clip, double layer_height, int scale)
-        {
-            Paths solution = new Paths();
-            
-            Clipper c = new Clipper();
-            c.AddPaths(contours, PolyType.ptSubject, false);
-            c.AddPaths(clip, PolyType.ptClip, true);
-
-            //WERKT NOG NIET
-
-            //Clipper.ZFillCallback customCallback = clipperCallback;
-            //c.ZFillFunction(customCallback);
-            
-            PolyTree pSol = new PolyTree();
-
-            bool succes = c.Execute(ClipType.ctIntersection, pSol,
-              PolyFillType.pftEvenOdd, PolyFillType.pftEvenOdd);
-
-            solution = Clipper.PolyTreeToPaths(pSol);
-            // solution = Clipper.CleanPolygons(solution);
-
-            return solution;
-        }
-
-        public static void clipperCallback(IntPoint bot1, IntPoint top1, IntPoint bot2, IntPoint top2, ref IntPoint pt)
-        {
-            pt.Z = 20000;
         }
 
         private GeometryModel3D FindLargestModel(Model3DGroup group)
@@ -172,7 +84,25 @@ namespace CompFab_Slicer
             }
             return maxModel;
         }
-    
+
+        private void Slice_Click(object sender, RoutedEventArgs e)
+        {
+            if(modelMesh != null)
+            {
+                Slicer slicer = new Slicer(meshBuilder, modelMesh);
+                var visualModel = VisualModel;
+                MeshGeometry3D mesh = slicer.Slice();
+
+                GeometryModel3D slice = new GeometryModel3D { Geometry = mesh, Material = yellowMaterial, BackMaterial = insideMaterial, Transform = new TranslateTransform3D(0, 50, 0) };
+                modelGroup.Children.Add(slice);
+
+                visualModel.Content = modelGroup;
+            }
+            else
+            {
+                return;
+            } 
+        }
 
         private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
         {
@@ -182,11 +112,6 @@ namespace CompFab_Slicer
         private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
         {
         
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
---
+++ CompFab Slicer/CompFab Slicer/Slicer.cs
@@ -0,0 +1,157 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using Path = System.Collections.Generic.List<ClipperLib.IntPoint>;
+using Paths = System.Collections.Generic.List<System.Collections.Generic.List<ClipperLib.IntPoint>>;
+using ClipperLib;
+
+namespace CompFab_Slicer
+{
+    public class Slicer
+    {
+        MeshBuilder meshBuilder;
+        MeshGeometry3D modelMesh;
+
+        int scale = 10000;
+        public Slicer(MeshBuilder meshBuilder, MeshGeometry3D modelMesh)
+        {
+            this.meshBuilder = meshBuilder;
+            this.modelMesh = modelMesh;
+        }
+
+        public MeshGeometry3D Slice()
+        {
+            Paths zPlane = createClippingPlane(scale);
+            PolyTree contours = createContoursTree(scale, modelMesh);
+            Paths intersectingPoints = getIntersectingContours(Clipper.PolyTreeToPaths(contours), 180, 0.2, scale);
+
+            Paths slice = intersect(intersectingPoints, zPlane, 0.2, scale);
+
+            for (int i = 0; i < slice.Count; i++)
+            {
+                List<Point3D> polygon = new List<Point3D>();
+                for (int j = 0; j < slice[i].Count; j++)
+                {
+                    Point3D temp = new Point3D((double)(slice[i][j].X) / scale, (double)(slice[i][j].Y) / scale, 0);
+                    polygon.Add(temp);
+                }
+                meshBuilder.AddPolygon(polygon);
+            }
+            var mesh = meshBuilder.ToMesh(true);
+
+            return mesh;
+        }
+
+        public PolyTree createContoursTree(int scale, MeshGeometry3D mesh)
+        {
+            PolyTree contours = new PolyTree();
+            System.Windows.Media.Int32Collection indices = mesh.TriangleIndices;
+
+            for (int i = 0; i < mesh.TriangleIndices.Count; i += 3)
+            {
+                PolyNode contour = new PolyNode();
+
+                Point3D vec_1 = mesh.Positions[indices[i]];
+                Point3D vec_2 = mesh.Positions[indices[i + 1]];
+                Point3D vec_3 = mesh.Positions[indices[i + 2]];
+
+                contour.Contour.Add(new IntPoint((long)(vec_1.X * scale), (long)(vec_1.Y * scale), (long)(vec_1.Z * scale)));
+                contour.Contour.Add(new IntPoint((long)(vec_2.X * scale), (long)(vec_2.Y * scale), (long)(vec_2.Z * scale)));
+                contour.Contour.Add(new IntPoint((long)(vec_3.X * scale), (long)(vec_3.Y * scale), (long)(vec_3.Z * scale)));
+
+                contours.AddChild(contour);
+            }
+
+            return contours;
+        }
+
+        public Paths createClippingPlane(int scale)
+        {
+            Paths zPlane = new Paths();
+            Path cPlane = new Path();
+            cPlane.Add(new IntPoint(-100 * scale, -100 * scale, (long)(0.2 * scale)));
+            cPlane.Add(new IntPoint(-100 * scale, 100 * scale, (long)(0.2 * scale)));
+            cPlane.Add(new IntPoint(100 * scale, 100 * scale, (long)(0.2 * scale)));
+            cPlane.Add(new IntPoint(100 * scale, -100 * scale, (long)(0.2 * scale)));
+
+            zPlane.Add(cPlane);
+
+            return zPlane;
+        }
+
+        public void checkIntersection(IntPoint pt1, IntPoint pt2, long zPlane, ref Path intersectingContours)
+        {
+            long zMax, zMin;
+
+            if (pt1.Z != pt2.Z)
+            {
+                if (pt1.Z < pt2.Z)
+                {
+                    zMin = pt1.Z;
+                    zMax = pt2.Z;
+                }
+                else
+                {
+                    zMin = pt2.Z;
+                    zMax = pt1.Z;
+
+                    IntPoint temp = pt1;
+                    pt1 = pt2;
+                    pt2 = temp;
+                }
+
+                if (zMin < zPlane && zMax > zPlane)
+                {
+                    Path pt = new Path();
+
+                    long X = pt1.X + (((zPlane - pt1.Z) * (pt2.X - pt1.X)) / (pt2.Z - pt1.Z));
+                    long Y = pt1.X + (((zPlane - pt1.Z) * (pt2.Y - pt1.Y)) / (pt2.Z - pt1.Z));
+                    long Z = zPlane;
+
+                    intersectingContours.Add(new IntPoint(X, Y, Z));
+                }
+            }
+        }
+
+
+        public Paths getIntersectingContours(Paths contours, int layerNr, double layer_height, int scale)
+        {
+            Paths intersectingContours = new Paths();
+            Path intersectingPoints = new Path();
+            long zPlane = (long)(layerNr * layer_height * scale);
+
+            for (int i = 0; i < contours.Count; i++)
+            {
+                checkIntersection(contours[i][0], contours[i][1], zPlane, ref intersectingPoints);
+                checkIntersection(contours[i][0], contours[i][2], zPlane, ref intersectingPoints);
+                checkIntersection(contours[i][1], contours[i][2], zPlane, ref intersectingPoints);
+            }
+            intersectingContours.Add(intersectingPoints);
+            return intersectingContours;
+        }
+
+        public Paths intersect(Paths contours, Paths clip, double layer_height, int scale)
+        {
+            Paths solution = new Paths();
+
+            Clipper c = new Clipper();
+            c.AddPaths(contours, PolyType.ptSubject, false);
+            contours.Clear();
+            c.AddPaths(clip, PolyType.ptClip, true);
+
+            PolyTree pSol = new PolyTree();
+
+            bool succes = c.Execute(ClipType.ctIntersection, pSol,
+              PolyFillType.pftNonZero, PolyFillType.pftNonZero);
+
+            solution = Clipper.PolyTreeToPaths(pSol);
+            //solution = Clipper.CleanPolygons(solution);
+
+            return solution;
+        }
+    }
+}
